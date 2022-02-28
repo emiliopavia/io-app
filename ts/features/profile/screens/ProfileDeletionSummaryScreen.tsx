8d8496ffe7e8c5aa1c9e028d8bfcc811ac87e698
Emilio Pavia <emilio.pavia@gmail.com>
2022-02-28 12:09:35 +0100

    Implement profile deletion request

--- ts/features/profile/screens/ProfileDeletionSummaryScreen.tsx
+++ ts/features/profile/screens/ProfileDeletionSummaryScreen.tsx
@@ -1,8 +1,14 @@
 import React from "react";
 import { Dispatch } from "redux";
 import { connect } from "react-redux";
-import { SafeAreaView } from "react-native";
+import {
+  ActivityIndicator,
+  SafeAreaView,
+  StyleSheet,
+  View
+} from "react-native";
 import { NavigationStackScreenProps } from "react-navigation-stack";
+import * as pot from "italia-ts-commons/lib/pot";
 import { GlobalState } from "../../../store/reducers/types";
 import { emptyContextualHelp } from "../../../utils/emptyContextualHelp";
 import BaseScreenComponent from "../../../components/screens/BaseScreenComponent";
@@ -11,6 +17,18 @@ import I18n from "../../../i18n";
 import FooterWithButtons from "../../../components/ui/FooterWithButtons";
 import { userProfileDeletionCancel } from "../actions";
 import UserProfileDetails from "../components/UserProfileDetails";
+import { upsertUserDataProcessing } from "../../../store/actions/userDataProcessing";
+import { UserDataProcessingChoiceEnum } from "../../../../definitions/backend/UserDataProcessingChoice";
+import { selectUserProfileDeletionStatus } from "../reducers/userProfile";
+import { showToast } from "../../../utils/showToast";
+
+const styles = StyleSheet.create({
+  activityIndicator: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center"
+  }
+});
 
 type Props = ReturnType<typeof mapDispatchToProps> &
   ReturnType<typeof mapStateToProps> &
@@ -28,29 +46,48 @@ const ProfileDeletionSummaryScreen = (props: Props): React.ReactElement => {
   const continueButtonProps = {
     block: true,
     primary: true,
-    onPress: undefined,
+    onPress: props.deleteUserProfile,
     title: I18n.t("global.buttons.continue")
   };
+
+  if (pot.isError(props.deletionStatus)) {
+    showToast(I18n.t("global.genericError"), "danger");
+  }
 
   return (
     <BaseScreenComponent goBack={true} contextualHelp={emptyContextualHelp}>
       <SafeAreaView style={IOStyles.flex}>
-        <UserProfileDetails></UserProfileDetails>
-        <FooterWithButtons
-          type="TwoButtonsInlineThird"
-          leftButton={cancelButtonProps}
-          rightButton={continueButtonProps}
-        />
+        {!pot.isUpdating(props.deletionStatus) && (
+          <>
+            <UserProfileDetails />
+            <FooterWithButtons
+              type="TwoButtonsInlineThird"
+              leftButton={cancelButtonProps}
+              rightButton={continueButtonProps}
+            />
+          </>
+        )}
+        {pot.isUpdating(props.deletionStatus) && (
+          <View style={styles.activityIndicator}>
+            <ActivityIndicator />
+          </View>
+        )}
       </SafeAreaView>
     </BaseScreenComponent>
   );
 };
 
 const mapDispatchToProps = (dispatch: Dispatch) => ({
-  cancel: () => dispatch(userProfileDeletionCancel())
+  cancel: () => dispatch(userProfileDeletionCancel()),
+  deleteUserProfile: () =>
+    dispatch(
+      upsertUserDataProcessing.request(UserDataProcessingChoiceEnum.DELETE)
+    )
 });
 
-const mapStateToProps = (state: GlobalState) => ({});
+const mapStateToProps = (state: GlobalState) => ({
+  deletionStatus: selectUserProfileDeletionStatus(state)
+});
 
 export default connect(
   mapStateToProps,
